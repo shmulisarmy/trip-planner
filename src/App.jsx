76ce3f8376fc573f5eb87050a1240ac20202e954
shmuel keller <shmuli@shmuels-MacBook-Air.local>
2024-10-21 15:17:36 -0400

    added components into components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { createSignal, For, Show, createResource } from "solid-js";
-import Events_C from "./features/events/component";
+import Events_C from "./features/events/components/events_container";
 import { change_mode, all_modes } from "./utils";
 
 import { place_holder_value } from "./on_start";
+import {toggle_collapse_button} from "./features/events/components/buttons/toggle_collapse"
 
 const [count, setCount] = createSignal(0);
 function App() {
   return (
     <>
+      {toggle_collapse_button}
       <button onclick={() => setCount(count() + 1)}>{count()}</button>
       {all_modes.map((mode) => (
         <button onclick={() => change_mode(mode)}>{mode} mode</button>
---
+++ src/features/events/components/buttons/toggle_collapse.tsx
@@ -0,0 +1,6 @@
+import { toggle_event_size_callapse } from "../../settings";
+
+
+
+
+export const toggle_collapse_button = <button onclick={toggle_event_size_callapse}>toggle collapse</button>
